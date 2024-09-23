cd318a1b34e7679d33fae542f39c1008df6ae927
AstryBrana <astry.brana@gmail.com>
2024-09-23 17:46:21 -0400

    added function d

--- function_d.py
+++ function_d.py
@@ -1,8 +1,9 @@
 def max_value(numbers):
-    """ This function returns the largest number
-        in the list.
-    """
-    pass
+    max_value = 0
+    for number in numbers:
+        if number > max_value:
+            max_value = number
+    return max_value
 
 
 if __name__ == "__main__":
